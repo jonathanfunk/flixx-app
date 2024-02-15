db399a7ab3e8c29ff9936b4f68afd80c55249755
Jonathan Funk <jon.m.funk@gmail.com>
2024-02-15 12:30:09 -0800

    added loading spinner

--- js/script.js
+++ js/script.js
@@ -40,13 +40,23 @@ async function fetchAPIData(endpoint) {
 	const API_KEY = '5cf7b79c47f0d7fd79b6409b5489d2ac';
 	const API_URL = 'https://api.themoviedb.org/3/';
 
+	showSpinner();
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
 	);
 
 	const data = await response.json();
 
+	hideSpinner();
 	return data;
+}
+
+function showSpinner() {
+	document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+	document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
