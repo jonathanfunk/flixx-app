b78837f154978b142295673868928fdd85422d7d
Jonathan Funk <jon.m.funk@gmail.com>
2024-02-15 12:38:45 -0800

    display popular tv shows on the shows page

--- js/script.js
+++ js/script.js
@@ -35,6 +35,40 @@ async function displayPopularMovies() {
 	});
 }
 
+// Display 20 most popular tv shows
+async function displayPopularShows() {
+	const { results } = await fetchAPIData('tv/popular');
+	results.forEach((show) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+        ${
+					show.poster_path
+						? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+						: `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+				}
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+		document.querySelector('#popular-shows').appendChild(div);
+	});
+}
+
 // Fetch data from TMBD API
 async function fetchAPIData(endpoint) {
 	const API_KEY = '5cf7b79c47f0d7fd79b6409b5489d2ac';
@@ -77,7 +111,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('Shows');
+			displayPopularShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie Details');
