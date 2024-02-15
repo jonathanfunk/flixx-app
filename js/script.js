e29481ff2d6fe2a2ccdfd16bd6bea6aa16ac52c2
Jonathan Funk <jon.m.funk@gmail.com>
2024-02-15 14:39:55 -0800

    added a slider to the homepage

--- js/script.js
+++ js/script.js
@@ -230,6 +230,54 @@ async function displayShowDetails() {
 	document.querySelector('#show-details').appendChild(div);
 }
 
+// Display Slider Movies
+async function displaySlider() {
+	const { results } = await fetchAPIData('movie/now_playing');
+
+	results.forEach((movie) => {
+		const div = document.createElement('div');
+		div.classList.add('swiper-slide');
+		div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}g" alt="${
+			movie.title
+		}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+					1
+				)} / 10
+      </h4>
+    `;
+		document.querySelector('.swiper-wrapper').appendChild(div);
+		initSwiper();
+	});
+}
+
+function initSwiper() {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2,
+			},
+			700: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	});
+}
+
 // Display Backdrop on Details Pages
 function displayBackgroundImage(type, backgroundPath) {
 	const overlayDiv = document.createElement('div');
@@ -294,6 +342,7 @@ function init() {
 	switch (global.currentPage) {
 		case '/':
 		case '/index.html':
+			displaySlider();
 			displayPopularMovies();
 			break;
 		case '/shows.html':
