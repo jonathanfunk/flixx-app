f3bb86bdd876178c9d3d154650d5ea7f75b1f0cb
Jonathan Funk <jon.m.funk@gmail.com>
2024-02-15 13:32:11 -0800

    added backdrop to movie details

--- js/script.js
+++ js/script.js
@@ -75,7 +75,8 @@ async function displayMovieDetails() {
 
 	const movie = await fetchAPIData(`movie/${movieId}`);
 
-	console.log(movie);
+	// Overlay for background image
+	displayBackgroundImage('movie', movie.backdrop_path);
 
 	const div = document.createElement('div');
 
@@ -150,6 +151,27 @@ async function displayMovieDetails() {
   `;
 
 	document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop on Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+	const overlayDiv = document.createElement('div');
+	overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+	overlayDiv.style.backgroundSize = 'cover';
+	overlayDiv.style.backgroundPosition = 'center';
+	overlayDiv.style.backgroundRepeat = 'no-repeat';
+	overlayDiv.style.height = '100vh';
+	overlayDiv.style.width = '100vw';
+	overlayDiv.style.position = 'absolute';
+	overlayDiv.style.top = '0';
+	overlayDiv.style.left = '0';
+	overlayDiv.style.zIndex = '-1';
+	overlayDiv.style.opacity = '0.1';
+	if (type === 'movie') {
+		document.querySelector('#movie-details').appendChild(overlayDiv);
+	} else {
+		document.querySelector('#show-details').appendChild(overlayDiv);
+	}
 }
 
 // Fetch data from TMBD API
